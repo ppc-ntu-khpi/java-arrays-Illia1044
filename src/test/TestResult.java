56edc5c931754ef30361509e8ca76c640736ebeb
Illia1044 <92012282+Illia1044@users.noreply.github.com>
2022-02-12 17:13:35 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,30 @@
-package test;
 
-import domain.Exercise;
+
 
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+      int a = 0;
+      int b = 20;
+      int[] Arr = new int[20]; 
+      System.out.println("Random Massive:");
+      for (int i=0; i<20;i++)
+      {
+        Arr[i]=Exercise.Calculate(a,b);
+        System.out.println(Arr[i]);
+      }
+      int[] Check = new int[20]; 
+      int c = 0;
+      for(int i = 0; i<20; i++)
+      {
+        c = Arr[i];
+        Check[c]++;
+        c=0;
+      }
+      System.out.println("Counts of number of Massive:" );
+      for(int i = 0; i<20;i++)
+      {
+        System.out.println("Count of number "+i+" in Arr = " +Check[i]);
+      }
     }
 }

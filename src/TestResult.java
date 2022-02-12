8f82f6b97d4b0f9ca14083037306704c4e3c8709
Illia1044 <92012282+Illia1044@users.noreply.github.com>
2022-02-12 17:17:30 +0200

    Add files via upload

---
+++ src/TestResult.java
@@ -0,0 +1,30 @@
+
+
+
+public class TestResult {
+
+    public static void main(String[] args) {
+      int a = 0;
+      int b = 20;
+      int[] Arr = new int[20]; 
+      System.out.println("Random Massive:");
+      for (int i=0; i<20;i++)
+      {
+        Arr[i]=Exercise.Calculate(a,b);
+        System.out.println(Arr[i]);
+      }
+      int[] Check = new int[20]; 
+      int c = 0;
+      for(int i = 0; i<20; i++)
+      {
+        c = Arr[i];
+        Check[c]++;
+        c=0;
+      }
+      System.out.println("Counts of number of Massive:" );
+      for(int i = 0; i<20;i++)
+      {
+        System.out.println("Count of number "+i+" in Arr = " +Check[i]);
+      }
+    }
+}

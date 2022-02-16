4c32e15667c162f26ae7f5f5210d6875182d2064
Illia1044 <92012282+Illia1044@users.noreply.github.com>
2022-02-16 20:15:33 +0200

    Add files via upload

--- src/Exercise.java
+++ src/Exercise.java
@@ -6,7 +6,40 @@ public class Exercise {
   *число b - максимальний поріг
   *Потім це число вписується у масив
   */
-    public static int Calculate(int a, int b){
-        return (int) (a + (int) (Math.random() * b));
+    public static String Calculate(){
+      int a = 0;
+      int b = 20;
+      int[] Arr = new int[20]; 
+      System.out.println("Random Massive:");
+      for (int i=0; i<20;i++)
+      {
+        Arr[i]=(a + (int) (Math.random() * b));
+        System.out.println(Arr[i]);
+      }
+      int[] Check = new int[20]; 
+      int c = 0;
+      for(int i = 0; i<20; i++)
+      {
+        c = Arr[i];
+        Check[c]++;
+        c=0;
+      }
+      int f=0;
+      System.out.println("Counts of number in Massive:" );
+      for(int i = 0; i<20;i++)
+      {
+        System.out.println("Count of number "+i+" in Arr = " +Check[i]);
+        if(Check[i]>f)f=Check[i];
+      }
+      
+      
+      
+      String maxnum="";
+      for (int i =0; i<20; i++)
+      {
+        if(Check[i]==f)
+        maxnum += Integer.toString(i) + ", ";
+      }
+      return maxnum;
     }
 }
